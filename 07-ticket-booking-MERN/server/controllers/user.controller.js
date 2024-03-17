3482fcf6f5c17e873c8fa9174628448a6279cbe1
shivsundar3009 <shivsundar3009@gmail.com>
2024-03-17 23:38:43 +0530

    bcrypt added and working 07ticketbooking

--- 07-ticket-booking-MERN/server/controllers/user.controller.js
+++ 07-ticket-booking-MERN/server/controllers/user.controller.js
@@ -1,19 +1,23 @@
 import { User } from "../models/user.model.js";
+import bcrypt from "bcrypt"
 
-export const Register = async (req,res) => {
+export const Register = async (req, res) => {
+  try {
+      const { name, email, password, age } = req.body;
 
+      // Check if the email already exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+          return res.status(400).json({ message: "User already exists" });
+      }
 
-    try {
-        const {name,email,password,age} = req.body
-        
-      const user = await User.create({name,email,password,age})
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-      res.status(201).json(user)
-
-        
-    } catch (error) {
-        
-    }
-   
-
+      // If email doesn't exist, create a new user with hashed password
+      const newUser = await User.create({ name, email, password: hashedPassword, age });
+      res.status(201).json(newUser);
+  } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+  }
 }
