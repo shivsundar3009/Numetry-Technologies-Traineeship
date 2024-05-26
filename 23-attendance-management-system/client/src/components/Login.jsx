5170382de747437ca7a9be891645c34f4a3147d7
shivsundar3009 <shivsundar3009@gmail.com>
2024-05-26 22:42:07 +0530

    css change

--- 23-attendance-management-system/client/src/components/Login.jsx
+++ 23-attendance-management-system/client/src/components/Login.jsx
@@ -39,7 +39,7 @@ const Login = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-cover" style={{ backgroundImage: `url(${schoolBackground})` }}>
-      <div className="bg-white p-8 rounded shadow-lg">
+      <div className="border-red-500 border-4 bg-white p-8 rounded shadow-lg">
         <h2 className="text-2xl mb-4">Welcome back to our site!</h2>
         <p className="text-gray-600 mb-4">Don't have an account? <Link to="/SignUP" className="text-blue-500 hover:underline">SignUP Here!</Link></p>
         <form onSubmit={handleSubmit}>
@@ -51,25 +51,45 @@ const Login = () => {
             <label className="block">Password:</label>
             <input type="password" name="password" value={password} onChange={handleChange} className="border border-gray-300 rounded px-4 py-2 w-full" />
           </div>
-          <div className="mb-4">
-            <label className="block">Role:</label>
-            <label className="mr-4">
-              <input type="checkbox" name="admin" checked={roles.admin} onChange={handleChange} className="mr-2" />
-              Admin
-            </label>
-            <label className="mr-4">
-              <input type="checkbox" name="staff" checked={roles.staff} onChange={handleChange} className="mr-2" />
-              Staff
-            </label>
-            <label className="mr-4">
-              <input type="checkbox" name="student" checked={roles.student} onChange={handleChange} className="mr-2" />
-              Student
-            </label>
-            <label className="mr-4">
-              <input type="checkbox" name="parent" checked={roles.parent} onChange={handleChange} className="mr-2" />
-              Parent
-            </label>
-          </div>
+          <p className="mb-1 font-medium text-gray-500">Select Your Role</p>
+    <div className="ROLE-DIV mb-6 flex flex-col gap-y-2 gap-x-4 lg:flex-row">
+      <div className="relative flex w-56 items-center justify-center rounded-xl bg-gray-50 px-4 py-3 font-medium text-gray-700">
+        <input className="peer hidden" type="radio" value="Projects" onChange={(e) => {
+          setRole(e.target.value)
+          console.log(e.target.value)
+          }} name="radio" id="radio1"/>
+        <label className="peer-checked:border-blue-600 peer-checked:bg-blue-200 absolute top-0 h-full w-full cursor-pointer rounded-xl border" htmlFor="radio1"> </label>
+        <div className="peer-checked:border-transparent peer-checked:bg-blue-600 peer-checked:ring-2 absolute left-4 h-5 w-5 rounded-full border-2 border-gray-300 bg-gray-200 ring-blue-600 ring-offset-2"></div>
+        <span className="pointer-events-none z-10">Projects</span>
+      </div>
+      <div className="relative flex w-56 items-center justify-center rounded-xl bg-gray-50 px-4 py-3 font-medium text-gray-700">
+        <input className="peer hidden" type="radio" value={'Staff'} onChange={(e) => {
+          setRole(e.target.value)
+          console.log(e.target.value)
+          }} name="radio" id="radio2"/>
+        <label className="peer-checked:border-blue-600 peer-checked:bg-blue-200 absolute top-0 h-full w-full cursor-pointer rounded-xl border" htmlFor="radio2"> </label>
+        <div className="peer-checked:border-transparent peer-checked:bg-blue-600 peer-checked:ring-2 absolute left-4 h-5 w-5 rounded-full border-2 border-gray-300 bg-gray-200 ring-blue-600 ring-offset-2"></div>
+        <span className="pointer-events-none z-10">Staff</span>
+      </div>
+      <div className="relative flex w-56 items-center justify-center rounded-xl bg-gray-50 px-4 py-3 font-medium text-gray-700">
+        <input className="peer hidden" type="radio" name="radio" id="radio3" value={'HR'} onChange={(e) => {
+          setRole(e.target.value)
+          console.log(e.target.value)
+          }}/>
+        <label className="peer-checked:border-blue-600 peer-checked:bg-blue-200 absolute top-0 h-full w-full cursor-pointer rounded-xl border" htmlFor="radio3"> </label>
+        <div className="peer-checked:border-transparent peer-checked:bg-blue-600 peer-checked:ring-2 absolute left-4 h-5 w-5 rounded-full border-2 border-gray-300 bg-gray-200 ring-blue-600 ring-offset-2"></div>
+        <span className="pointer-events-none z-10">HR</span>
+      </div>
+      <div className="relative flex w-56 items-center justify-center rounded-xl bg-gray-50 px-4 py-3 font-medium text-gray-700">
+        <input className="peer hidden" type="radio" name="radio" id="radio4" value={'Employee'} onChange={(e) => {
+          setRole(e.target.value)
+          console.log(e.target.value)
+          }}/>
+        <label className="peer-checked:border-blue-600 peer-checked:bg-blue-200 absolute top-0 h-full w-full cursor-pointer rounded-xl border" htmlFor="radio4"> </label>
+        <div className="peer-checked:border-transparent peer-checked:bg-blue-600 peer-checked:ring-2 absolute left-4 h-5 w-5 rounded-full border-2 border-gray-300 bg-gray-200 ring-blue-600 ring-offset-2"></div>
+        <span className="pointer-events-none z-10">Employee</span>
+      </div>
+    </div>
           <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Login</button>
         </form>
       </div>
